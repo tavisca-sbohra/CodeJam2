37bd95004692e293d140d22ca43a7bdcfd1630ef
sbohra <sbohra@gmail.com>
2014-07-07 12:00:17 +0530

    complete solution

---
+++ Shivangi/Shivangi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeJam
+{
+    class ACGT
+    {
+        int MinChanges(int maxPeriod, string[] acgt)
+        {
+            //Your code goes here
+            return 0;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            ACGT aCGT = new ACGT();
+            String input = Console.ReadLine();
+            do
+            {
+                var inputParts = input.Split('|');
+                int maxPeriod = int.Parse(inputParts[0]);
+                string[] acgt = inputParts[1].Split(',');
+                Console.WriteLine(aCGT.MinChanges(maxPeriod, acgt));
+                input = Console.ReadLine();
+            } while (input != "-1");
+        }
+        #endregion
+    }
+}
